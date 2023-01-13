c48e0b24360420e29906b62db60a426b4334ec97
Faeez Miller <faeezmiller8@gmail.com>
2023-01-13 10:53:34 +0200

    newer

---
+++ src/components/NavBar.vue
@@ -0,0 +1,54 @@
+<template>
+   <nav class="w3-sidebar w3-bar-block w3-small w3-hide-small w3-center">
+    <!-- Avatar image in top left corner -->
+    <img src="https://i.postimg.cc/0j6Ss9NY/mr-robot.jpg" class="mrrobot" style="width:100% ">
+
+
+    <a href="#" class="w3-bar-item w3-button w3-padding-large w3-black">
+      
+      <h3>HOME</h3>
+    </a>
+    <a href="#about" class="w3-bar-item w3-button w3-padding-large w3-black">
+      
+      <h3>ABOUT</h3>
+    </a>
+    <a href="#testimonials" class="w3-bar-item w3-button w3-padding-large w3-black">
+      
+      <h3>TESTIMONIALS</h3>
+    </a>
+    <a href="#projects" class="w3-bar-item w3-button w3-padding-large w3-black">
+      
+      <h3>PROJECTS</h3>
+    </a>
+    <a href="#photos" class="w3-bar-item w3-button w3-padding-large w3-black">
+      
+      <h3>PHOTOS</h3>
+    </a>
+    <a href="#resume" class="w3-bar-item w3-button w3-padding-large w3-black">
+      
+      <h3>RESUME</h3>
+    </a>
+    <a href="#contact" class="w3-bar-item w3-button w3-padding-large w3-black">
+      
+      <h3>CONTACT</h3>
+    </a>
+  <!-- Navbar -->
+  <div class="w3-top w3-hide-large w3-hide-medium" id="myNavbar">
+    <div class="w3-bar w3-black w3-opacity w3-hover-opacity-off w3-center w3-small">
+      <a href="#home" class="w3-bar-item w3-button" style="width:25% !important">HOME</a>
+      <a href="#about" class="w3-bar-item w3-button" style="width:25% !important">ABOUT</a>
+      <a href="#testimonials" class="w3-bar-item w3-button" style="width:25% !important">TESTIMONIALS</a>
+      <a href="#projects" class="w3-bar-item w3-button" style="width:25% !important">PROJECTS</a>
+      <a href="#photos" class="w3-bar-item w3-button" style="width:25% !important">PHOTOS</a>
+      <a href="#resume" class="w3-bar-item w3-button" style="width:25% !important">RESUME</a>
+      <a href="#contact" class="w3-bar-item w3-button" style="width:25% !important">CONTACT</a>
+    </div>
+  </div>
+  </nav> 
+</template>
+
+<script>
+export default{
+    name:'NavBar'
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,55 +9,11 @@
   <link rel="shortcut icon" href="https://i.postimg.cc/4NhTzjcV/smiley-PNG10.png" type="image/x-icon">
   
  </head>
- <!-- Icon Bar (Sidebar - hidden on small screens) -->
-  <nav class="w3-sidebar w3-bar-block w3-small w3-hide-small w3-center">
-    <!-- Avatar image in top left corner -->
-    <img src="https://i.postimg.cc/0j6Ss9NY/mr-robot.jpg" class="mrrobot" style="width:100% ">
-
-
-    <a href="#" class="w3-bar-item w3-button w3-padding-large w3-black">
-      
-      <h3>HOME</h3>
-    </a>
-    <a href="#about" class="w3-bar-item w3-button w3-padding-large w3-black">
-      
-      <h3>ABOUT</h3>
-    </a>
-    <a href="#testimonials" class="w3-bar-item w3-button w3-padding-large w3-black">
-      
-      <h3>TESTIMONIALS</h3>
-    </a>
-    <a href="#projects" class="w3-bar-item w3-button w3-padding-large w3-black">
-      
-      <h3>PROJECTS</h3>
-    </a>
-    <a href="#photos" class="w3-bar-item w3-button w3-padding-large w3-black">
-      
-      <h3>PHOTOS</h3>
-    </a>
-    <a href="#resume" class="w3-bar-item w3-button w3-padding-large w3-black">
-      
-      <h3>RESUME</h3>
-    </a>
-    <a href="#contact" class="w3-bar-item w3-button w3-padding-large w3-black">
-      
-      <h3>CONTACT</h3>
-    </a>
+ 
   
-  </nav>
+  <NavBar/>
 
-  <!-- Navbar -->
-  <div class="w3-top w3-hide-large w3-hide-medium" id="myNavbar">
-    <div class="w3-bar w3-black w3-opacity w3-hover-opacity-off w3-center w3-small">
-      <a href="#home" class="w3-bar-item w3-button" style="width:25% !important">HOME</a>
-      <a href="#about" class="w3-bar-item w3-button" style="width:25% !important">ABOUT</a>
-      <a href="#testimonials" class="w3-bar-item w3-button" style="width:25% !important">TESTIMONIALS</a>
-      <a href="#projects" class="w3-bar-item w3-button" style="width:25% !important">PROJECTS</a>
-      <a href="#photos" class="w3-bar-item w3-button" style="width:25% !important">PHOTOS</a>
-      <a href="#resume" class="w3-bar-item w3-button" style="width:25% !important">RESUME</a>
-      <a href="#contact" class="w3-bar-item w3-button" style="width:25% !important">CONTACT</a>
-    </div>
-  </div>
+  
 
   <body class="w3-black">
   <!-- Page Content -->
@@ -72,11 +28,14 @@
   </div>
 
   <body>
-     <div class="-padding-64 w3-content w3-text-light-grey" id="about">
+  </body>
+ 
+   <!-- About Section -->
+   
+   <div class="-padding-64 w3-content w3-text-light-grey" id="about">
     </div>
-      <h2 class="w3-padding-16 w3-text-light-grey">About</h2>
-      <hr style="width:155px" class="w3-opacity">
-      
+    <h2 class="w3-padding-16 w3-text-light-grey">About</h2>
+    <hr style="width:155px" class="w3-opacity">
 
         <p>I am an enthusiastic, self-motivated, reliable, responsible and hard working person. I am a mature team
           worker and adaptable to all challenging situations. I am able to work well both in a team environment as well
@@ -95,59 +54,57 @@
       <h2 class="w3-padding-16 w3-text-light-grey">My Skills</h2>
       <hr style="width:150px" class="w3-opacity">
       <!--skill bars-->
-      <p class="w3-wide">JAVASCRIPT</p>
+      <p class="w3-wide">JAVASCRIPT<i class="fa-brands fa-html5"></i></p>
       <div class="w3-white">
         <div class="w3-dark-grey" style="height:28px;width:40%"></div>
       </div>
       <br>
-      <p class="w3-wide">CSS</p>
-      <div class="w3-white">
-        <div class="w3-dark-grey" style="height:28px;width:70%"></div>
-      </div>
-      <br>
-      <p class="w3-wide">BOOTSTRAP</p>
+      <p class="w3-wide">CSS<i class="fa-brands fa-css3-alt"></i></p>
       <div class="w3-white">
         <div class="w3-dark-grey" style="height:28px;width:60%"></div>
       </div>
+      <br>
+      <p class="w3-wide">BOOTSTRAP<i class="fa-brands fa-bootstrap"></i></p>
+      <div class="w3-white">
+        <div class="w3-dark-grey" style="height:28px;width:70%"></div>
+      </div>
       
       <div class="w3-quarter w3-section">
-        <span class="w3-xlarge">6+</span><br>
+        <span class="w3-xlarge">1+</span><br>
         Projects Done
       </div>
       <div class="w3-quarter w3-section">
-        <span class="w3-xlarge">5+</span><br>
+        <span class="w3-xlarge">2+</span><br>
         Happy Clients
       </div>
       <br>
-      <br>
-      <br>
-
+   
       
   
-</body>
+    <br>
+    <br>
+    <br>
     
-      <br>
-      
-      <div class="-padding-64 w3-content w3-text-light-grey" id="testimonials">
+    <div class="-padding-64 w3-content w3-text-light-grey" id="testimonials">
     </div>
-      <h2 class="w3-padding-16 w3-text-light-grey">Testimonials</h2>
-      <hr style="width:155px" class="w3-opacity">
-      
-      <figure class="snip1192">
-  <blockquote>Faeez is a hardworking, focused individual. I have a deep respect for his work ethic.</blockquote>
-  <div class="author">
-    <img src="https://i.postimg.cc/YC9CDfqX/3gwnhdk9.png" alt="@John Noad"/>
-    <h5>John Noad<span> Software Developer</span></h5>
+    <h2 class="w3-padding-16 w3-text-light-grey">Testimonials</h2>
+    <hr style="width:155px" class="w3-opacity">
+    
+    <figure class="testimonial">
+      <blockquote>Faeez is a hardworking, focused individual. I have a deep respect for his work ethic.</blockquote>
+      <div class="author">
+        <img src="https://i.postimg.cc/YC9CDfqX/3gwnhdk9.png" alt="@John Noad"/>
+        <h5>John Noad<span> Software Developer</span></h5>
   </div>
 </figure>
-      <figure class="snip1192">
+      <figure class="testimonial">
   <blockquote>Faeez Miller is a down to earth, kind ,creative mature colleague. I highly admire his work ethic and passion.</blockquote>
   <div class="author">
     <img src="https://i.postimg.cc/Fs7p907q/cam.jpg" alt="@Camryn Herandien"/>
     <h5>Camryn Herandien <span> Software Developer</span></h5>
   </div>
 </figure>
-<figure class="snip1192 hover">
+<figure class="testimonial">
   <blockquote>Faeez is hardworking, meticulous. I can proudly say his approach towards
             work is always steadfast, which gets the task done without wasting time.</blockquote>
   <div class="author">
@@ -155,21 +112,21 @@
     <h5>Deno Rautenbach<span> Software Developer</span></h5>
   </div>
 </figure>
-<figure class="snip1192">
+<figure class="testimonial">
   <blockquote>Faeez is a funny and chilled guy. He is reliable and never hesitates to help out the next person. He truly has a heart of gold.</blockquote>
   <div class="author">
     <img src="https://i.postimg.cc/tRhk795d/a5l25dbb.png" alt="@Mu-een Slamat"/>
     <h5>Mu-een Slamat<span> Software Developer</span></h5>
   </div>
 </figure>
-<figure class="snip1192">
+<figure class="testimonial">
   <blockquote>Faeez is attentive in his ability to listen to other's and asses what can be done. He is not afraid to put in the time and effort required to get the results he is looking for. </blockquote>
   <div class="author">
     <img src="https://i.postimg.cc/L86FNCX7/77t9r27u.png" alt="@Matthew Thomas"/>
     <h5>Matthew Thomas<span> Software Developer</span></h5>
   </div>
 </figure>
-<figure class="snip1192">
+<figure class="testimonial">
   <blockquote>Faeez is a highly thought intellectual from my perspective. He is not only hardworking but ambitious to recreate his vision exactly as thought of.</blockquote>
   <div class="author">
     <img src="https://i.postimg.cc/DyWf3QXm/8fp54qi8.png" alt="@Aneeqa Brown"/>
@@ -177,11 +134,11 @@
   </div>
 </figure>
 
-          <br>
-          <br>
-          <br>
-          <br>
-          <br>
+<br>
+<br>
+<br>
+<br>
+<br>
           <br>
           <br>
           <br>
@@ -206,20 +163,22 @@
           <br>
           <br>
           
-       <br>
+          <br>
        
           <div class="-padding-64 w3-content w3-text-light-grey" id="projects">
     </div>
     <h2 class="w3-padding-16 w3-text-light-grey">Projects</h2>
     <hr style="width:120px" class="w3-opacity">
-           
+    
+    
+    
+    
     <div class="row">
-              
-              <div class="col">
-                <div class="card bg-dark text-white" style="width: 18rem ;">
-                  <img src="https://i.postimg.cc/C1qcBkqd/8f7687531ae2dc2d2436d157cbae5657.jpg" class="nasa" alt="...">
-                  <div class="card-body">
-                    <center>
+      <div class="col">
+        <div class="card bg-dark text-white" style="width: 18rem ;">
+          <img src="https://i.postimg.cc/C1qcBkqd/8f7687531ae2dc2d2436d157cbae5657.jpg" class="nasa" alt="...">
+          <div class="card-body">
+            <center>
                       <h5 class="card-title">NASAtrips</h5>
                     </center>
                     <p class="card-text"></p>
@@ -474,18 +433,17 @@
 
             </body >
           </template>
+          
+        
 
   <style>
   
   body {
-  background-color: #100e17;
-  font-family: 'Open Sans', sans-serif;
-}
-
-
-
+       background-color: #100e17;
+       font-family: 'Open Sans', sans-serif;
+       }
   .mrrobot{
-    height: 235px;
+       height: 235px;
   }
   .picture2{
     height: 600px;
@@ -514,51 +472,31 @@
     padding: px;
     background-color: black;
   }
-    body,
-    h1,
-    h2,
-    h3,
-    h4,
-    h5,
-    h6 {
-      font-family: "Montserrat", "sans-serif"
-    }
-  
-    .w3-row-padding img {
+  .w3-row-padding img {
       margin-bottom: 12px
-    }
-  
-    /* Set the width of the sidebar to 120px */
-    .w3-sidebar {
+  }
+  /* Set the width of the sidebar to 120px */
+  .w3-sidebar {
       width: 110px;
       height: 100%;
       background: #222;
-    }
-  
-    /* Add a left margin to the "page content" that matches the width of the sidebar (120px)
-    #main {
-      margin-left: 120px
-    } */
-  
-    /* Remove margins from "page content" on small screens */
-    @media only screen and (max-width: 600px) {
+  }
+   @media only screen and (max-width: 600px) {
       #main {
         margin-left: 0
       }
-    }
-    
-    
-.main-timeline:after{
+  }
+  .main-timeline:after{
     display: block;
     clear: both;
-}
+  }
 .main-timeline .timeline{
     width: calc(50% + 58px);
     border-top: 2px solid white;
     float: left;
     position: relative;
     z-index: 1;
-}
+  }
 .main-timeline .timeline:before{
     background: white;
     width: 10px;
@@ -567,14 +505,14 @@
     position: absolute;
     top: -5px;
     right: 0;
-}
+  }
 .main-timeline .timeline-content{
     color: white;
     text-align: center;
     padding: 15px 80px 15px 10px;
     display: block;
     position: relative;
-}
+  }
 .main-timeline .timeline-content:hover{ text-decoration: none; }
 .main-timeline .timeline-content:before{
     content: "";
@@ -584,7 +522,7 @@
     position: absolute;
     top: 0;
     right: 50px;
-}
+  }
 .main-timeline .timeline-year{
     font-size: 35px;
     line-height: 35px;
@@ -593,7 +531,7 @@
     position: absolute;
     top: 50%;
     right: -15px;
-}
+  }
 .main-timeline .title{
     font-size: 22px;
     font-weight: 600;
@@ -628,7 +566,7 @@
     .main-timeline .timeline{ width: 100%; }
 }
 
-figure.snip1192 {
+figure.testimonial {
   font-family: 'Raleway', Arial, sans-serif;
   position: relative;
   float: left;
@@ -641,11 +579,11 @@ figure.snip1192 {
   text-align: left;
   box-shadow: none !important;
 }
-figure.snip1192 * {
+figure.testimonial * {
   -webkit-box-sizing: border-box;
   box-sizing: border-box;
 }
-figure.snip1192 img {
+figure.testimonial img {
   max-width: 100%;
   height: 100px;
   width: 100px;
@@ -655,7 +593,7 @@ figure.snip1192 img {
   z-index: 1;
   position: relative;
 }
-figure.snip1192 blockquote {
+figure.testimonial blockquote {
   margin: 0;
   display: block;
   border-radius: 8px;
@@ -668,8 +606,8 @@ figure.snip1192 blockquote {
   line-height: 1.6em;
   box-shadow: 0 0 5px rgba(0, 0, 0, 0.15);
 }
-figure.snip1192 blockquote:before,
-figure.snip1192 blockquote:after {
+figure.testimonial blockquote:before,
+figure.testimonial blockquote:after {
   font-family: 'FontAwesome';
   content: "\201C";
   position: absolute;
@@ -677,33 +615,46 @@ figure.snip1192 blockquote:after {
   opacity: 0.3;
   font-style: normal;
 }
-figure.snip1192 blockquote:before {
+figure.testimonial blockquote:before {
   top: 35px;
   left: 20px;
 }
-figure.snip1192 blockquote:after {
+figure.testimonial blockquote:after {
   content: "\201D";
   right: 20px;
   bottom: 35px;
 }
-figure.snip1192 .author {
+figure.testimonial .author {
   margin: 0;
   text-transform: uppercase;
   text-align: center;
   color: #ffffff;
 }
-figure.snip1192 .author h5 {
+figure.testimonial .author h5 {
   opacity: 0.8;
   margin: 0;
   font-weight: 800;
 }
-figure.snip1192 .author h5 span {
+figure.testimonial .author h5 span {
   font-weight: 400;
   text-transform: none;
   display: block;
 }
 
   </style>
+
+  <script>
+   import NavBar from '../components/NavBar.vue'
+   export default{
+    components:{
+      NavBar
+    }
+   }
+   
+  </script>
+  
+  
+
 
 
 
